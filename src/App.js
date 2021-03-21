990c48d3a2e8b6dde44ed2c94182b8684754571a
con-mcmon <connormccafferty1@gmail.com>
2021-03-21 15:38:16 -0700

    Nodes rendered in consistent positions on route image

--- src/App.js
+++ src/App.js
@@ -1,45 +1,134 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import churningInTheWake from './content/images/churning-in-the-wake.jpg';
+import routes from './content/routes';
 import circle from './content/images/circle.png';
 import rightHand from './content/images/hand-right.png';
 import leftHand from './content/images/hand-left.png';
 import rightFoot from './content/images/foot-right.png';
 import leftFoot from './content/images/foot-left.png';
 
+function intToPx(num) {
+  return num + 'px';
+}
+
+function percentToPx(x, y, imageWidth, imageHeight) {
+  return {
+    x: (x / 100) * imageWidth,
+    y: (y / 100) * imageHeight
+  }
+}
+
+function pxToPercent(x, y, imageWidth, imageHeight) {
+  return {
+    x: (x / imageWidth) * 100,
+    y: (y / imageHeight) * 100
+  }
+}
+
 class App extends Component {
   constructor(props) {
     super();
     this.state = {
-      image: churningInTheWake,
-      onImage: false,
-      coordinates: {
-        x: 0,
-        y: 0
-      },
-      icons: [],
-      toolBox: false
+      route: routes.churningInTheWake,
     }
   }
 
-  handleMouseOver = (e) => {
-    this.setState({ onImage: true });
+  render() {
+    return (
+      <div className="App" >
+        <Route route={this.state.route} />
+      </div>
+    );
+  }
+}
+
+class Route extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      coordinates: {
+        x: 0,
+        y:0
+      },
+      imageOffset: {
+        x: 0,
+        y: 0
+      },
+      imageWidth: 0,
+      imageHeight: 0,
+      onImage: false,
+      touchPoints: [],
+      toolBox: null,
+      crux: null
+    }
+    this.image = React.createRef();
   }
 
+  componentDidMount = () => {
+    window.addEventListener('resize', this.updateImageDimensions);
+  }
+
+  componentWillUnmount = () => {
+    window.removeEventListener('resize', this.updateImageDimensions);
+  }
+
+  updateImageDimensions = () => {
+    this.setState({
+      imageWidth: this.image.current.width,
+      imageHeight: this.image.current.height });
+  }
+
+  renderCruxNodes = () => {
+    return this.props.route.crux.map(({ coords, image, alt }) => {
+      const { x, y } = percentToPx(coords.x, coords.y, this.state.imageWidth, this.state.imageHeight);
+      return <CruxNode
+                coordinates={{ x:x, y:y }}
+                image={image}
+                alt={alt}
+                key={alt} />
+              })
+            }
+
+  renderTouchPoints = () => {
+    return this.state.touchPoints.map(({ type, x, y }, index) => {
+      return <TouchPoint
+                key={index}
+                type={type}
+                coordinates={{ x:x, y:y }}
+                imageWidth={this.state.imageWidth}
+                imageHeight={this.state.imageHeight} />
+              })
+            }
+
+  handleImageLoad = (e) => {
+    this.setState({
+      imageWidth: e.target.width,
+      imageHeight: e.target.height });
+  }
+
+  handleMouseOver = (e) => {
+    this.setState({
+      onImage: true,
+      imageOffset: {
+        x: e.target.offsetParent.offsetLeft,
+        y: e.target.offsetParent.offsetTop }
+      });
+    }
+
   handleMouseLeave = (e) => {
-    console.log('mouse out');
     this.setState({ onImage: false });
   }
 
   handleClick = (e) => {
-    this.setState({ toolBox: <ToolBox coordinates={{ x:e.clientX, y:e.clientY }} handleClick={this.handleToolBoxClick} /> });
+    this.setState({ toolBox: <ToolBox coordinates={{ x:e.nativeEvent.offsetX, y:e.nativeEvent.offsetY }} handleClick={this.handleToolBoxClick} /> });
   }
 
-  handleToolBoxClick = (value, x, y) => {
+  handleToolBoxClick = (type, x, y) => {
+    const percentValues = pxToPercent(x, y, this.state.imageWidth, this.state.imageHeight);
     this.setState((prevState) => (
       {
-        icons: [...prevState.icons, <Icon value={value} coordinates={{ x:x, y:y }} key={[x.toString(), y.toString()].join(',')} /> ],
-        toolBox: false
+        touchPoints: [...prevState.touchPoints, {type: type, x:percentValues.x, y:percentValues.y}],
+        toolBox: null
       }
     ))
   }
@@ -55,35 +144,40 @@ class App extends Component {
   render() {
     const { x, y } = this.state.coordinates;
     return (
-      <div className="App" onMouseMove={this.handleMouseMove}>
+      <div>
         <h1>{ x } { y }</h1>
-        <img
-          src={this.state.image}
-          alt="churning in the wake"
-          onMouseOver={this.handleMouseOver}
-          onMouseLeave={this.handleMouseLeave}
-          onClick={this.handleClick} />
-        {this.state.onImage ? <ImageZoom coordinates={{ x:this.state.coordinates.x, y:this.state.coordinates.y }} /> : null}
-        {this.state.icons}
-        {this.state.toolBox}
+        <div className='route' onMouseOver={this.handleMouseOver} >
+          <img
+            className='route-img'
+            ref={this.image}
+            onLoad={this.handleImageLoad}
+            src={this.props.route.image}
+            alt={this.props.route.alt}
+            onMouseMove={this.handleMouseMove}
+            onMouseLeave={this.handleMouseLeave}
+            onClick={this.handleClick} />
+          {this.renderCruxNodes()}
+          {this.renderTouchPoints()}
+          {this.state.toolBox}
+        </div>
       </div>
-    );
+      )
+    }
   }
-}
 
-function ImageZoom(props) {
-  return (
-    <div className='image-zoom' style={{ left:props.coordinates.x + 'px', top:props.coordinates.y + 'px' }}>
-    </div>
-  )
-}
+// {this.state.onImage ? <ImageZoom coordinates={{ x:this.state.coordinates.x, y:this.state.coordinates.y }} /> : null}
+// function ImageZoom(props) {
+//   return (
+//     <div className='image-zoom' style={{ left:props.coordinates.x + 'px', top:props.coordinates.y + 'px' }}>
+//     </div>
+//   )
+// }
 
-function Icon(props) {
-  const x = props.coordinates.x + 'px';
-  const y = props.coordinates.y + 'px';
+function TouchPoint(props) {
+  const { x, y } = percentToPx(props.coordinates.x, props.coordinates.y, props.imageWidth, props.imageHeight);
 
   let icon;
-  switch (props.value) {
+  switch (props.type) {
     case 'rightFoot':
       icon = rightFoot;
       break;
@@ -100,7 +194,13 @@ function Icon(props) {
       icon = null;
       break;
   }
-  return <img src={icon} className='icon' alt='icon' style={{ left:x, top:y }} />;
+  return (
+    <img
+      src={icon}
+      className='touch-point'
+      alt='icon'
+      style={{ left:intToPx(x), top:intToPx(y) }} />
+    )
 }
 
 class ToolBox extends Component {
@@ -117,7 +217,7 @@ class ToolBox extends Component {
 
   render() {
     return (
-      <div className='tool-box' style={{ left:this.x + 'px', top:this.y + 'px' }}>
+      <div className='tool-box' style={{ left:intToPx(this.x), top:intToPx(this.y) }}>
         <button onClick={this.handleClick} value={'rightFoot'}>Right Foot</button>
         <button onClick={this.handleClick} value={'leftFoot'}>Left Foot</button>
         <button onClick={this.handleClick} value={'rightHand'}>Right Hand</button>
@@ -125,6 +225,16 @@ class ToolBox extends Component {
       </div>
     )
   }
+}
+
+function CruxNode(props) {
+  return (
+    <img
+      src={circle}
+      alt={props.alt}
+      className='crux-node'
+      style={{ left:intToPx(props.coordinates.x), top:intToPx(props.coordinates.y) }} />
+    )
 }
 
 export default App;
---
+++ src/content/routes.js
@@ -0,0 +1,16 @@
+import churningInTheWake from './images/churning-in-the-wake.jpg';
+import churningInTheWakeCrux from './images/churning-in-the-wake_crux.jpg';
+
+export default {
+  churningInTheWake: {
+    image: churningInTheWake,
+    alt: 'churning in the wake',
+    crux: [
+    {
+      coords: {x:50, y:50},
+      image: churningInTheWakeCrux,
+      alt: 'churning in the wake crux'
+    },
+    ]
+  }
+}
