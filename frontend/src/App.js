838a4bf9d1246915e6bf707bfb254e16c77af490
con-mcmon <connormccafferty1@gmail.com>
2021-04-17 10:55:44 -0700

    decoupled Route and Dashboards and rendered inside Content

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { Component, createRef, useState, useRef } from 'react';
+import { Component, createRef, useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import './App.css';
 import { toPx, percentToPx, pxToPercent } from './helpers';
@@ -7,51 +7,83 @@ import { BetaDashboard, Beta } from './beta';
 import { useDivCenter } from './hooks';
 import styles from './style';
 
-class App extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      routes: [],
-      route: null,
-      crux: null,
-      holds: []
+function App(props) {
+  const [routes, setRoutes] = useState([]);
+  const [selectedRoute, setSelectedRoute] = useState(null);
+
+  useEffect(() => {
+    const getRoutes = () => {
+      axios.get('/routes')
+        .then((res) => setRoutes(res.data))
+        .catch((err) => console.error(err))
+      }
+    getRoutes();
+    }, [])
+
+  const renderRouteList = () => {
+    return (
+      <div>
+        {routes.map(({ name, id }) => {
+          return (
+            <button
+              id={id}
+              onClick={handleRouteClick}
+              key={name} >
+              {name}
+            </button> )
+            })}
+      </div> )
     }
-  }
 
-  componentDidMount = () => {
-    this.getRoutes();
-    document.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount = () => {
-    document.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  getRoutes = () => {
-    axios.get('/routes')
-      .then((res) => this.setState({ routes: res.data }))
+  const handleRouteClick = (e) => {
+    axios.get(`/routes/${e.target.id}`)
+      .then((res) => setSelectedRoute(res.data))
       .catch((err) => console.error(err))
-  }
-
-  handleKeyDown = (e) => {
-    if (e.keyCode === 27) {
-      this.setState({ crux: null });
     }
-  }
 
-  renderRoute = (parent) => {
+  return (
+    <div className='App' >
+      <h1>Beta Builder</h1>
+      {renderRouteList()}
+      {selectedRoute ? <Content route={selectedRoute} /> : null}
+    </div>
+  )
+}
+
+function Content(props) {
+  const [holds, setHolds] = useState([]);
+  const [selectedHold, setSelectedHold] = useState(null);
+  const [beta, setBeta] = useState([]);
+  const [crux, setCrux] = useState(null);
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.keyCode === 27) {
+        setCrux(null);
+      }
+    }
+    document.addEventListener('keydown', handleKeyDown);
+    return document.removeEventListener('keydown', handleKeyDown);
+    }, [])
+
+  useEffect(() => {
+    setBeta([]);
+    setHolds([]);
+    }, [props.route])
+
+  const renderRoute = (parent) => {
     let params = {};
-    const route = this.state.route;
+    const route = props.route;
     if (parent) {
       params.name = route.name;
       params.image = route.img;
       params.crux= route.crux;
-      params.cruxOpen = this.state.crux;
+      params.cruxOpen = crux;
       params.style = {};
     } else {
-      const crux = route.crux[this.state.crux];
+      const crux = route.crux[crux];
       params.cruxOpen = null;
-      params.name = this.state.crux;
+      params.name = crux;
       params.image = crux.image;
       params.alt = crux.alt;
       params.style = { width: '40%', marginTop: '5%' };
@@ -59,81 +91,76 @@ class App extends Component {
     return <Route
               parent={parent}
               name={params.name}
+              id={route._id}
               image={params.image}
-              holds={this.state.holds}
-              beta={route.beta}
+              holds={holds}
+              selectedHold={selectedHold}
               crux={params.crux}
               style={params.style}
-              handleCruxClick={this.handleCruxClick}
-              handleCloseClick={this.handleCruxCloseClick}
+              beta={beta}
+              handleCruxClick={(name) => setCrux(name)}
+              handleCloseClick={() => setCrux(null)}
               cruxOpen={params.cruxOpen}
-              handleMouseMove={this.handleHoldMove}
-              holds={this.state.holds.filter(({ parent }) => parent === route.name)}
-              addHold={this.addHold}
-              deleteHold={this.deleteHold}
-              handleHoldNote={this.handleHoldNote}
-              swapHoldPositions={this.swapHoldPositions} />
+              handleMouseMove={handleHoldMove}
+              holds={holds.filter(({ parent }) => parent === route.name)}
+              addHold={addHold}
+              deleteHold={deleteHold}
+              handleHoldNote={handleHoldNote}
+              swapHoldPositions={swapHoldPositions}
+              handleHoldMouseOver={handleHoldMouseOver} />
   }
 
-  handleCruxClick = (name) => this.setState({ crux: name });
-
-  handleCruxCloseClick = () => this.setState({ crux: null });
-
-  //Hold
-  addHold = (type, x, y, parent) => {
+  const addHold = (type, x, y, parent) => {
     const newHold = {
-      id: this.nextHoldID(),
+      id: nextHoldID(),
       type: type,
       coordinates: {x: x, y: y},
       parent: parent,
       note: '',
-      position: this.nextHoldPosition(parent)
+      position: nextHoldPosition(parent)
     };
-    this.setState((state) => ({ holds: [...state.holds, newHold] }));
+    setHolds((holds) => [...holds, newHold]);
   }
 
-  nextHoldID = () => {
+  const nextHoldID = () => {
     //increment last id of all holds
-    const ids = this.state.holds.map(({ id }) => id);
-    return this.state.holds.length > 0 ? Math.max(...ids) + 1 : 0;
+    const ids = holds.map(({ id }) => id);
+    return holds.length > 0 ? Math.max(...ids) + 1 : 0;
   }
 
-  nextHoldPosition = (parent) => {
+  const nextHoldPosition = (parent) => {
     //increment last position of holds in parent
-    const siblingPositions = this.state.holds
+    const siblingPositions = holds
                               .filter((hold) => hold.parent === parent)
                               .map(({ position }) => position);
     return siblingPositions.length > 0 ? Math.max(...siblingPositions) + 1 : 0;
   }
 
-  deleteHold = (holdID) => {
-    this.setState((state) => ({ holds: state.holds.filter(({ id }) => id !== holdID) }));
-    this.shiftHoldPositions(holdID);
+  const deleteHold = (holdID) => {
+    setHolds((holds) => holds.filter(({ id }) => id !== holdID) );
+    shiftHoldPositions(holdID);
   }
 
-  shiftHoldPositions = (holdID) => {
-    const position = this.state.holds.find(({ id }) => id === holdID).position;
-    this.setState((state) => ({ holds: state.holds.map((hold) => hold.position > position ? {...hold, position: hold.position - 1} : hold) }));
+  const shiftHoldPositions = (holdID) => {
+    const position = holds.find(({ id }) => id === holdID).position;
+    setHolds((holds) => holds.map((hold) => hold.position > position ? {...hold, position: hold.position - 1} : hold));
   }
 
-  handleHoldMove = (holdID, x, y) => {
-    this.setState((prevState) => ({
-      holds: prevState.holds.map((hold) => hold.id === holdID ? { ...hold, coordinates: {x: x, y: y} } : hold)
-      }))
+  const handleHoldMove = (holdID, x, y) => {
+    setHolds((holds) => holds.map((hold) => hold.id === holdID ? { ...hold, coordinates: {x: x, y: y} } : hold));
     }
 
-  handleHoldNote = (holdID, value) => {
-    this.setState((prevState) => ({ holds: prevState.holds.map((hold) => hold.id === holdID ? { ...hold, note: value } : hold) }))
+  const handleHoldNote = (holdID, value) => {
+    setHolds((holds) => holds.map((hold) => hold.id === holdID ? { ...hold, note: value } : hold))
   }
 
-  swapHoldPositions = (holdID, targetHoldID) => {
-    const currentPosition = this.state.holds.find(({ id }) => id === holdID).position;
-    const targetPosition = this.state.holds.find(({ id }) => id === targetHoldID).position;
+  const swapHoldPositions = (holdID, targetHoldID) => {
+    const currentPosition = holds.find(({ id }) => id === holdID).position;
+    const targetPosition = holds.find(({ id }) => id === targetHoldID).position;
 
-    this.setState((prevState) => {
-      const holds = prevState.holds;
-      return {
-        holds: holds.map((hold) => {
+    setHolds((holds) => {
+      return (
+        holds.map((hold) => {
           if (hold.id === holdID) {
             return {...hold, position: targetPosition}
           }
@@ -142,52 +169,37 @@ class App extends Component {
           }
           return hold;
           })
-        }
-      })
+          )
+        })
+      }
+
+  const handleHoldMouseOver = (id, hovered) => hovered ? setSelectedHold(id) : setSelectedHold(null);
+
+  const handleBetaClick = (e) => {
+    const id = e.target.id;
+    const routeBeta = props.route.beta.find(({ _id }) => _id === id);
+    //don't render duplicate beta on Route
+    if (!beta.find(({ _id }) => _id === id)) {
+      setBeta((beta) => [...beta, routeBeta]);
     }
-
-  renderRouteList = () => {
-    const routes = this.state.routes.map(({ name, id }) => {
-      return (
-        <button
-          id={id}
-          onClick={this.handleRouteClick}
-          key={name} >
-          {name}
-        </button> )
-      })
-
-    return (
-      <div>
-        {routes}
-      </div> )
-    }
-
-  handleRouteClick = (e) => {
-    axios.get(`/routes/${e.target.id}`)
-      .then((res) => this.setState({ route: res.data, crux: null }))
-      .catch((err) => console.error(err))
   }
 
-  handleUploadBetaClick = () => {
-    const holds = this.state.holds.filter((hold) => hold.parent === this.state.route.name);
-    axios.post(`/beta/${this.state.route._id}`, holds)
-      .then((res) => console.log(res))
-      .catch((err) => console.error(err))
-  }
-
-  render() {
-    const route = this.state.route;
-    return (
-      <div className='App' >
-        <h1>Beta Builder</h1>
-        {this.renderRouteList()}
-        {route ? <button onClick={this.handleUploadBetaClick}>Upload Beta</button> : null}
-        {route ? this.renderRoute(true) : null}
-        {this.state.crux !== null ? this.renderRoute(false) : null}
+  return (
+    <div className='content'>
+      {renderRoute(true)}
+      {crux !== null ? renderRoute(false) : null}
+      <div className='dashboards' >
+        <HoldDashboad
+          holds={holds.filter(({ parent }) => parent === props.route.name)}
+          selectedHold={selectedHold}
+          handleMouseOver={handleHoldMouseOver}
+          handleNoteChange={handleHoldNote}
+          handleDeleteClick={deleteHold}
+          swapHoldPositions={swapHoldPositions} />
+        <BetaDashboard beta={props.route.beta} handleBetaClick={handleBetaClick} />
       </div>
-    );
-  }
+    </div>
+    )
 }
 
 class Route extends Component {
@@ -200,8 +212,6 @@ class Route extends Component {
       },
       onImage: false,
       toolBox: null,
-      hold: null,
-      renderedBeta: []
     }
     this.image = createRef();
   }
@@ -243,13 +253,13 @@ class Route extends Component {
                   key={id}
                   id={id}
                   position={position}
-                  hovered={this.state.hold === id}
+                  hovered={this.props.selectedHold === id}
                   type={type}
                   coordinates={{ x:coords.x, y:coords.y }}
                   note={note}
                   containerDimensions={{ x: this.state.imageDimensions.x, y: this.state.imageDimensions.y}}
                   handleMouseMove={this.handleMouseMove}
-                  handleMouseOver={this.handleHoldMouseOver}
+                  handleMouseOver={this.props.handleHoldMouseOver}
                   handleDeleteClick={this.deleteHold} /> })
     }
 
@@ -262,14 +272,10 @@ class Route extends Component {
     this.props.handleMouseMove(id, x, y);
   }
 
-  handleHoldMouseOver = (id, hovered) => hovered ? this.setState({ hold: id }) : this.setState({ hold: null })
-
-  handleHoldNote = (id, value) => this.props.handleHoldNote(id, value);
-
   swapHoldID = (id, down) => this.props.swapHoldID(id, down);
 
   //Beta
-  renderBeta = () => this.state.renderedBeta.map(({ _id, holds }) => <Beta id={_id} holds={holds} key={_id} imageDimensions={this.state.imageDimensions} />);
+  renderBeta = () => this.props.beta.map(({ _id, holds }) => <Beta id={_id} holds={holds} key={_id} imageDimensions={this.state.imageDimensions} />);
 
   //ToolBox
   handleClick = (e) => {
@@ -287,50 +293,29 @@ class Route extends Component {
 
   closeToolBox = () => this.setState({ toolBox: null });
 
-  renderHoldDashboard = () => {
-    if (this.props.cruxOpen === null) {
-      return (
-        <HoldDashboad
-          holds={this.props.holds}
-          selectedHold={this.state.hold}
-          handleMouseOver={this.handleHoldMouseOver}
-          handleNoteChange={this.handleHoldNote}
-          handleDeleteClick={this.deleteHold}
-          addHold={this.props.addHold}
-          swapHoldPositions={this.props.swapHoldPositions} />
-        )
-    }
-    return null;
-  }
-
-  handleBetaClick = (e) => {
-    const id = e.target.id;
-    const beta = this.props.beta.find(({ _id }) => _id === id);
-    if (!this.state.renderedBeta.find(({ _id }) => _id === id)) {
-      this.setState((state) => ({ renderedBeta: [...state.renderedBeta, beta] }));
-    }
+  handleUploadBetaClick = () => {
+    axios.post(`/beta/${this.props.id}`, this.props.holds)
+      .then((res) => console.log(res))
+      .catch((err) => console.error(err))
   }
 
   render() {
     return (
-      <div className='content'>
-        <div className='route' style={this.props.style} >
-          <img
-            ref={this.image}
-            onLoad={this.handleImageLoad}
-            src={this.props.image}
-            alt={this.props.name}
-            onMouseOver={ () => this.setState({ onImage: true }) }
-            onMouseLeave={ () => this.setState({ onImage: false }) }
-            onClick={this.handleClick} />
-          {this.props.crux ? this.renderCruxs() : null}
-          {this.renderHolds()}
-          {this.renderBeta()}
-          {this.state.toolBox}
-          {!this.props.parent ? <button onClick={this.props.handleCloseClick}>Close</button> : null}
-        </div>
-        <BetaDashboard beta={this.props.beta} handleBetaClick={this.handleBetaClick} />
-        {this.renderHoldDashboard()}
+      <div className='route' style={this.props.style} >
+        <img
+          ref={this.image}
+          onLoad={this.handleImageLoad}
+          src={this.props.image}
+          alt={this.props.name}
+          onMouseOver={ () => this.setState({ onImage: true }) }
+          onMouseLeave={ () => this.setState({ onImage: false }) }
+          onClick={this.handleClick} />
+        {this.props.crux ? this.renderCruxs() : null}
+        {this.renderHolds()}
+        {this.renderBeta()}
+        {this.state.toolBox}
+        {!this.props.parent ? <button onClick={this.props.handleCloseClick}>Close</button> : null}
+        <button onClick={this.handleUploadBetaClick}>Upload Beta</button>
       </div>
       )
     }
